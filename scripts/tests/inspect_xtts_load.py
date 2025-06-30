baf303b21ff13492bcddd498fbc86c0e95e1458a
JackBoun1123 <hopboy553@gmail.com>
2025-06-30 12:13:47 +0700

    chore: remove non-production docs/scripts/tests

--- scripts/tests/inspect_xtts_load.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import inspect
-from TTS.tts.models.xtts import Xtts
-print(inspect.signature(Xtts.load_checkpoint))
-# Print first lines of source
-source_lines = inspect.getsource(Xtts.load_checkpoint).splitlines()[:20]
-for line in source_lines:
-    print(line)
